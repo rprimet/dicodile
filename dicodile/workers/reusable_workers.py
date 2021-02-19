db84d3a9c7e392c24abe3dd93419b7e027fa751d
Romain PRIMET <romain.primet@inria.fr>
2021-02-19 10:48:48 +0100

    flake8 again

--- dicodile/workers/reusable_workers.py
+++ dicodile/workers/reusable_workers.py
@@ -47,8 +47,8 @@ def get_reusable_workers(n_workers=4, hostfile=None):
         # util.Finalize(None, shutdown_reusable_workers, exitpriority=20)
     else:
         if _workers.n_workers != n_workers:
-            warnings.warn("Should not require different number of workers " \
-                          f"(expected: {_workers.n_workers}," \
+            warnings.warn("Should not require different number of workers "
+                          f"(expected: {_workers.n_workers},"
                           f"required: {n_workers})")
             shutdown_reusable_workers()
             del _workers
